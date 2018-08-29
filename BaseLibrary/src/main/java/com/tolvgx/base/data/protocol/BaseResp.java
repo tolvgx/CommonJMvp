c3420df16d786416fc0eaaac8506c1ddd011c6a2
Tolvgx <tolvgx@163.com>
2018-08-29 18:31:53 +0800

    响应基类实现

--- BaseLibrary/src/main/java/com/tolvgx/base/data/protocol/BaseResp.java
+++ BaseLibrary/src/main/java/com/tolvgx/base/data/protocol/BaseResp.java
@@ -2,14 +2,13 @@ package com.tolvgx.base.data.protocol;
 
 /**
  * Created by tolvgx on 2018/8/22.
+ *
+ * 能用响应对象
+ * @status:响应状态码
+ * @message:响应文字消息
+ * @data:具体响应业务对象
  */
 
-/*
-    能用响应对象
-    @status:响应状态码
-    @message:响应文字消息
-    @data:具体响应业务对象
- */
 public class BaseResp<T> {
     private int status;
     private String message;
