d61e8ebe1bf9529cd5f170b0de04ab0e2676ccb9
Tolvgx <tolvgx@163.com>
2018-09-10 17:09:17 +0800

    AlertUtils实现各种需求show()

--- BaseLibrary/src/main/java/com/tolvgx/base/utils/AlertUtils.java
+++ BaseLibrary/src/main/java/com/tolvgx/base/utils/AlertUtils.java
@@ -1,6 +1,7 @@
 package com.tolvgx.base.utils;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import com.tolvgx.base.R;
 
@@ -26,5 +27,62 @@ public class AlertUtils {
 
     public static AlertDialog.Builder createDefault(Context context){
         return new AlertDialog.Builder(context);
+    }
+
+    /**
+     * 显示内容
+     * @param context Context
+     * @param msg String
+     */
+    public static void show(Context context, String msg){
+        if (msg!=null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.shrinkAlertDialog)
+                    .setMessage(msg)
+                    .setCancelable(false)
+                    .setPositiveButton("确认", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int i) {
+                            dialog.cancel();
+                        }
+                    });
+            builder.show();
+        }
+    }
+
+    /**
+     * 显示内容，确认点击事件
+     * @param context Context
+     * @param msg String
+     * @param onPositiveListener OnClickListener
+     */
+    public static void show(Context context, String msg,
+                            DialogInterface.OnClickListener onPositiveListener){
+        if (msg!=null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.shrinkAlertDialog)
+                    .setMessage(msg)
+                    .setCancelable(false)
+                    .setPositiveButton("确认", onPositiveListener);
+            builder.show();
+        }
+    }
+
+    /**
+     * 显示内容，确认点击事件，取消点击事件
+     * @param context Context
+     * @param msg String
+     * @param onPositiveListener OnClickListener
+     * @param onNegativeListener OnClickListener
+     */
+    public static void show(Context context, String msg,
+                            DialogInterface.OnClickListener onPositiveListener,
+                            DialogInterface.OnClickListener onNegativeListener){
+        if (msg!=null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.shrinkAlertDialog)
+                    .setMessage(msg)
+                    .setCancelable(false)
+                    .setPositiveButton("确认", onPositiveListener)
+                    .setNegativeButton("取消", onNegativeListener);
+            builder.show();
+        }
     }
 }
